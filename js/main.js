b1f637fb236ef807a290424a35d8373a6f7f3a4d
Daniil <skarnovichdaniil@gmail.com>
2022-03-03 14:25:39 +0300

    Исправления функций

--- js/main.js
+++ js/main.js
@@ -1,23 +1,16 @@
 // решение было взято с сайта https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
   if (min >= 0 && max >= 0 && max > min) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   } else {
-    return console.log('Неправильный диапазон чисел')
+    throw new Error('Неправильный диапазон чисел');
   }
-}
+};
 
-function checkСommentLength(enteredСomment, maxLength){
-  if (enteredСomment.length <= maxLength){
-    return true
-  } else {
-    return false
-  }
-}
+const checkСommentLength = (str, maxLength) => str.length <= maxLength;
 
-console.log(getRandomInt(5, 5))
-
-
+getRandomInt();
+checkСommentLength();
