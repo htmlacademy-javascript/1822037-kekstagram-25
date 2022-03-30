825ecb5ed3c1fb25954c21128578ec245d907605
Daniil <skarnovichdaniil@gmail.com>
2022-03-30 14:15:51 +0300

    отрисовка исправления

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import './data.js';
-import './renderPicture.js';
+import {renderPictures} from './renderPicture.js';
 import {createPhotos} from './data.js';
 import {createComments} from './data.js';
 import {checkСommentLength} from './util.js';
 
-createPhotos();
 createComments();
-checkСommentLength();
+checkСommentLength('строка', 12);
+
+const pictures = createPhotos();
+renderPictures(pictures);
--- js/renderPicture.js
+++ js/renderPicture.js
@@ -1,18 +1,18 @@
-import {createPhotos} from './data.js';
-
 const pictureList = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const randomPicture = createPhotos();
+const renderPictures = (pictures) => {
+  const pictureListFragment = document.createDocumentFragment();
 
-const pictureListFragment = document.createDocumentFragment();
+  pictures.forEach((photo) => {
+    const picture = pictureTemplate.cloneNode(true);
+    picture.querySelector('.picture__img').src = photo.url;
+    picture.querySelector('.picture__comments').textContent = photo.comments;
+    picture.querySelector('.picture__likes').textContent = photo.likes;
+    pictureListFragment.appendChild(picture);
+  });
 
-randomPicture.forEach((photo) => {
-  const picture = pictureTemplate.cloneNode(true);
-  picture.querySelector('.picture__img').src = photo.url;
-  picture.querySelector('.picture__comments').textContent = photo.comments;
-  picture.querySelector('.picture__likes').textContent = photo.likes;
-  pictureListFragment.appendChild(picture);
-});
+  pictureList.appendChild(pictureListFragment);
+};
 
-pictureList.appendChild(pictureListFragment);
+export {renderPictures};
