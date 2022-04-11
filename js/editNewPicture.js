891976d0528a513a30a47b1810bc430dc8d8a92d
Daniil <skarnovichdaniil@gmail.com>
2022-04-11 19:41:59 +0300

    исправления

--- js/editNewPicture.js
+++ js/editNewPicture.js
@@ -1,66 +1,108 @@
+let size = 1;
+const step = 0.25;
+const effectNames = {
+  none: {
+    options: {
+
+    }
+  },
+  chrome: {
+    filterName: 'grayscale',
+    unit: '',
+    options: {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      start: 1,
+      step: 0.1,
+      connect: 'lower',
+    },
+  },
+  sepia: {
+    filterName: 'sepia',
+    unit: '',
+    options: {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      start: 1,
+      step: 0.1,
+      connect: 'lower',
+    },
+  },
+  marvin: {
+    filterName: 'invert',
+    unit: '',
+    options: {
+      range: {
+        min: 0,
+        max: 100,
+      },
+      start: 100,
+      step: 1,
+      connect: 'lower',
+    },
+  },
+  phobos: {
+    filterName: 'blur',
+    unit: '',
+    options: {
+      range: {
+        min: 0,
+        max: 3,
+      },
+      start: 3,
+      step: 0.1,
+      connect: 'lower',
+    },
+  },
+
+  heat: {
+    filterName: 'brightness',
+    unit: '',
+    options: {
+      range: {
+        min: 1,
+        max: 3,
+      },
+      start: 3,
+      step: 0.1,
+      connect: 'lower',
+    },
+  },
+};
 const scaleControlSmallerElement = document.querySelector('.scale__control--smaller');
 const scaleControlBiggerElement = document.querySelector('.scale__control--bigger');
 const scaleControlInputElement = document.querySelector('.scale__control--value');
+const imgUploadScaleContainer = document.querySelector('.img-upload__scale');
 const fullPictureImg = document.querySelector('.img-upload__preview img');
 const effectsListElement = document.querySelector('.effects__list');
 const effectLevelContainer = document.querySelector('.img-upload__effect-level');
 const effectLevelSliderElement = document.querySelector('.effect-level__slider');
 const effectLevelValueElement = document.querySelector('.effect-level__value');
 
-
 scaleControlBiggerElement.setAttribute('disabled', 'disabled');
-
-let size = 1;
-const step = 0.25;
-
-scaleControlSmallerElement.addEventListener('click', () => {
-  size = size - step;
-  fullPictureImg.style  = `transform: scale(${size})`;
-  scaleControlBiggerElement.removeAttribute('disabled');
-  if (size === 0.25) {
-    scaleControlSmallerElement.setAttribute('disabled', 'disabled');
+const onfullPictureImgResize = (evt) => {
+  if (evt.target.closest('.scale__control--smaller')) {
+    size = size - step;
+    fullPictureImg.style.transform = `scale(${size})`;
+    scaleControlBiggerElement.removeAttribute('disabled');
+    if (size === 0.25) {
+      scaleControlSmallerElement.setAttribute('disabled', 'disabled');
+    }
+  } else if (evt.target.closest('.scale__control--bigger')) {
+    if (size < 1) {
+      size = size + step;
+      fullPictureImg.style  = `transform: scale(${size})`;
+      scaleControlSmallerElement.removeAttribute('disabled');
+    } else if (size === 1) {
+      scaleControlBiggerElement.setAttribute('disabled', 'disabled');
+    }
   }
-
-  const sizeString = size.toString();
-
-  if (sizeString.startsWith('1')) {
-    scaleControlInputElement.value = `${sizeString}00%`;
-  } else if(sizeString.endsWith('5') && sizeString.length === 3) {
-    scaleControlInputElement.value = `${sizeString.substring(2)}0%`;
-  } else {
-    scaleControlInputElement.value = `${sizeString.substring(2)}%`;
-  }
-});
-
-scaleControlBiggerElement.addEventListener('click', () => {
-  if (size < 1) {
-    size = size + step;
-    fullPictureImg.style  = `transform: scale(${size})`;
-    scaleControlSmallerElement.removeAttribute('disabled');
-  } else if (size === 1) {
-    scaleControlBiggerElement.setAttribute('disabled', 'disabled');
-  }
-
-  const sizeString = size.toString();
-
-  if (sizeString.startsWith('1')) {
-    scaleControlInputElement.value = `${sizeString}00%`;
-  } else if(sizeString.endsWith('5') && sizeString.length === 3) {
-    scaleControlInputElement.value = `${sizeString.substring(2)}0%`;
-  } else {
-    scaleControlInputElement.value = `${sizeString.substring(2)}%`;
-  }
-});
-
-let oldEffectsClass = fullPictureImg.classList.add('effects__preview--none');
-
-effectsListElement.addEventListener('change', (evt) => {
-  if (evt.target.closest('.effects__radio')) {
-    const newEffectClass = `effects__preview--${evt.target.value}`;
-    oldEffectsClass = fullPictureImg.classList.value;
-    fullPictureImg.classList.replace(oldEffectsClass, newEffectClass);
-  }
-});
+  scaleControlInputElement.value = `${(size * 100)}%`;
+};
 
 noUiSlider.create(effectLevelSliderElement, {
   range: {
@@ -72,45 +114,18 @@ noUiSlider.create(effectLevelSliderElement, {
   connect: 'lower',
 });
 
+imgUploadScaleContainer.addEventListener('click', onfullPictureImgResize);
+
+effectsListElement.addEventListener('change', (evt) => {
+  if (evt.target.closest('.effects__radio')) {
+    fullPictureImg.classList.value = `effects__preview--${evt.target.value}`;
+  }
+});
+
 effectsListElement.addEventListener('change', (evt) => {
   effectLevelContainer.classList.remove('hidden');
-  if (evt.target.closest('.effects__radio') && evt.target.value === 'marvin') {
-    effectLevelSliderElement.noUiSlider.updateOptions({
-      range: {
-        min: 0,
-        max: 100
-      },
-      start: 100,
-      step: 1
-    });
-  } else if (evt.target.closest('.effects__radio') && evt.target.value === 'phobos') {
-    effectLevelSliderElement.noUiSlider.updateOptions({
-      range: {
-        min: 0,
-        max: 3
-      },
-      start: 3,
-      step: 0.1
-    });
-  } else if (evt.target.closest('.effects__radio') && evt.target.value === 'heat') {
-    effectLevelSliderElement.noUiSlider.updateOptions({
-      range: {
-        min: 1,
-        max: 3
-      },
-      start: 3,
-      step: 0.1
-    });
-  } else {
-    effectLevelSliderElement.noUiSlider.updateOptions({
-      range: {
-        min: 0,
-        max: 1,
-      },
-      start: 1,
-      step: 0.1
-    });
-    effectLevelSliderElement.noUiSlider.set(1);
+  if (evt.target.closest('.effects__radio')) {
+    effectLevelSliderElement.noUiSlider.updateOptions(effectNames[evt.target.value].options);
   }
 });
 
